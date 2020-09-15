ba82bde4fb51d52c2bc30a18512d816c1fbd9150
subha81788 <subha81788@gmail.com>
2020-09-15 16:00:54 +0530

    initial commit

---
+++ src/test/java/org/subhashis/myconfigserver/MyConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.subhashis.myconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
